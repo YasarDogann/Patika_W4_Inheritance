d531765a159188b189ec5404fde9e29c3f09164b
YasarDogann <yasardgn99@gmail.com>
2024-10-18 11:08:30 +0300

    inheritance

---
+++ patika_w4_Inheritance/BasePeople.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_Inheritance
+{
+    public abstract class BasePeople  // Abstract class oluşturduk kendi başına kullanılmayacak bir class
+    {
+        public string Name { get; set; } // properties olarak direkt set get olarak tanımladık arka planda fields olarak tanımladı.
+        public string LastName { get; set; }
+
+        public virtual void PeopleInfo()  // Virtual bir method oluşturduk burada miras alan classlardan override ederek kullanılmasını değişiklik yapılabilr
+        {
+            Console.WriteLine($"{Name}" + " " + "{LastName}"); //bütün insanlarda isim soyisim olacağı için burada tanımladık
+        }
+    }
+
+    public class Teacher : BasePeople  //Teacher classı oluşturduk ve BasePeople classından miras aldı
+    {
+        public int Salary {  get; set; }  //properties oluşturduk
+        public override void PeopleInfo()  // BasePeople classından miras aldığımız için methoduna eriştik ve değişiklik eklemek için override yaptık
+        {
+            Console.WriteLine($"Öğretmen Adı : {Name}\r\nSoyadı : {LastName}\r\nMaaşı : {Salary}");
+
+        }
+    }
+
+    public class Student : BasePeople
+    {
+        public int Number { get; set; }
+        public override void PeopleInfo() // BasePeople classından miras aldığımız için methoduna eriştik ve değişiklik eklemek için override yaptık
+        {
+            Console.WriteLine($"Öğrenci Adı : {Name}\r\nSoyadı : {LastName}\r\nNumarası : {Number}");
+        }
+    }
+}
---
+++ patika_w4_Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace patika_w4_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student(); //Student class'ından bir nesne oluşturduk.
+            s.Name = "Abuzer";
+            s.LastName = "Kömürcü";
+            s.Number = 1296;
+            s.PeopleInfo(); //Abstract class içindeki PeopleInfo methodunu değişritebilmek için virtual yaptık ve student classında override ederk kullandık
+
+            Console.WriteLine("\r\n**********************\r\n");
+
+            Teacher teacher = new Teacher();  //Teacher classından bir nesne oluşturduk  ve bilgileri atadık
+            teacher.Name = "Muratgilin";
+            teacher.LastName = "Damı";
+            teacher.Salary = 1500;
+            teacher.PeopleInfo();
+         
+        }
+    }
+}
